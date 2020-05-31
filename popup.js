2940427bc39d3b9c2514e86e32adfa0ee643e52c
JZillifro <zillifrojacob@gmail.com>
2020-05-30 21:08:16 -0500

    wall texture spacing fixed, experimenting with minecraft textures

--- popup.js
+++ popup.js
@@ -1,19 +1,19 @@
-function fill_wall(name) {
-  for(var i = 0; i < 25; i++) {
+function fill_wall(wall_id, texture) {
+  for(var i = 0; i < 16; i++) {
     var block = document.createElement("img");
-    block.src = "https://tr.rbxcdn.com/6473dbb20816fe2a17dafd5d01438242/420/420/Decal/Png";
+    block.src = texture;
     block.className = "wall_block";
-    var wall = document.getElementById(name);
+    var wall = document.getElementById(wall_id);
     wall.appendChild(block);
   }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  fill_wall("wall_back");
-  fill_wall("wall_top");
-  fill_wall("wall_bottom");
-  fill_wall("wall_right");
-  fill_wall("wall_left");
+  fill_wall("wall_back", "https://tr.rbxcdn.com/6473dbb20816fe2a17dafd5d01438242/420/420/Decal/Png");
+  fill_wall("wall_top", "https://pbs.twimg.com/media/DwQIdH2XcAAvpO4.jpg");
+  fill_wall("wall_bottom", "https://lh3.googleusercontent.com/HUlj1PBI0tiE9_XQyhpLuQmJ67NKrRLR6R4vEbSjxVuQJIo4_eFPwHZ-b-OPZSgdAeq0HzfsQ_6vVayappEB7g=s400");
+  fill_wall("wall_right", "https://tr.rbxcdn.com/6473dbb20816fe2a17dafd5d01438242/420/420/Decal/Png");
+  fill_wall("wall_left", "https://tr.rbxcdn.com/6473dbb20816fe2a17dafd5d01438242/420/420/Decal/Png");
 
 });
